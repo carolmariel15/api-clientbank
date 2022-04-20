ad70d24491a238b6b2cf524333916fe4fd975d5f
Carol Ñavincopa <cnavinco@nttdata.com>
2022-04-20 17:30:59 -0500

    integracion de api-bankaccount

--- src/main/java/com/nttdata/apliclient/util/Constants.java
+++ src/main/java/com/nttdata/apliclient/util/Constants.java
@@ -2,9 +2,11 @@ package com.nttdata.apliclient.util;
 
 public interface Constants {
 	
-public static final String PATH_SERVICE_TRANSACTION  ="http://localhost:8085";
-public static final String PATH_SERVICE_TRANSACTION_URI  ="/api/transaction/client/";
-// @formatter:on
- ; 
+	public static final String PATH_SERVICE_TRANSACTION = "http://localhost:8085";
+	public static final String PATH_SERVICE_TRANSACTION_URI = "/api/transaction/client/";
+
+	public static final String PATH_SERVICE_BANKACCOUNT = "http://localhost:8082";
+	public static final String PATH_SERVICE_BANKACCOUNT_URI = "/api/bankaccount";
+
 
 }
